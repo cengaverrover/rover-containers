64c823d820b1fe1ffa144090766ab768ff057295
Dena <94480307+DenavDot@users.noreply.github.com>
2024-11-23 19:55:03 +0300

    Delete src directory

--- src/imu/as.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source /opt/ros/humble/setup.bash
-source /home/install/setup.bash
-
-exec ros2 run jetson_imu bno055_publisher /dev/i2c-1 0x28 $@
--- src/lidar/as.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source /opt/ros/humble/setup.bash
-source /home/install/setup.bash
-
-exec ros2 launch rplidar_ros rplidar.launch.py $@
