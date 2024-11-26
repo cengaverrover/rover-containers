c0665d4916ee42854ebe9456f82ccebf10b90aa8
Dena <94480307+DenavDot@users.noreply.github.com>
2024-11-26 22:41:41 +0300

    Changed IMU publisher

--- build/imu/as.sh
+++ build/imu/as.sh
@@ -2,4 +2,4 @@
 source /opt/ros/humble/setup.bash
 source /home/install/setup.bash
 
-exec ros2 run jetson_imu bno055_publisher /dev/i2c-1 0x28 $@
+exec ros2 run bno055_ros2 bno055_publisher /dev/i2c-1 0x28 $@
