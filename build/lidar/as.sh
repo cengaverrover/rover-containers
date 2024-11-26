6412bfe7d7f12cfe8eee497c26010eb62c577f20
Dena <94480307+DenavDot@users.noreply.github.com>
2024-11-26 22:48:14 +0300

    add support for filtering lidar

--- build/lidar/as.sh
+++ build/lidar/as.sh
@@ -3,4 +3,4 @@
 source /opt/ros/humble/setup.bash
 source /home/install/setup.bash
 
-exec ros2 launch rplidar_ros rplidar.launch.py $@
+exec ros2 launch rplidar_ros rplidar.launch.py --ros-args -r /scan:=/scan_raw -p frame_id:=laser_frame $@
